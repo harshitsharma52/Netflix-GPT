da5eb8b0064915c08900f833f74897759ac0bbbf
harshitsharma52 <harshit.sharma6091@email.com>
2023-08-28 20:45:38 +0530

    validation,redux and auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,14 +3,19 @@ import Header from './Header'
 import { checkValidData } from '../utils/validate';
 import { auth } from '../utils/firebase';
 
-import {createUserWithEmailAndPassword,signInWithEmailAndPassword} from "firebase/auth";
+import {createUserWithEmailAndPassword,signInWithEmailAndPassword,updateProfile} from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Login = () => {
  
  const [isSignInForm,setIsSignForm]=useState(true);
  const[errorMessage,setErrorMessage]=useState();
+ const navigate=useNavigate();
+ const dispatch=useDispatch();
 
-//const name=useRef(null);
+ const name=useRef(null);
  const email=useRef(null);
  const password=useRef(null);
 
@@ -19,57 +24,65 @@ const Login = () => {
     setIsSignForm(!isSignInForm);
  }
 
- const handleButtonClick=()=>{
-
-    //validate data
-    const message=checkValidData(email.current.value,password.current.value);
-    
+ const handleButtonClick = () => {
+    const message = checkValidData(email.current.value, password.current.value);
     setErrorMessage(message);
+    if (message) return;
 
-    //sign in/up
-    if(message!=null) return; //if we get error then no need to create a new user or sign in/up
-
-    //sign in/up logic
-
-    if(!isSignInForm)  //sign up logic
-    {
-     createUserWithEmailAndPassword(auth,email.current.value,password.current.value)
-     .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      console.log(user);
-      // ...
-     })
-     .catch((error) => {
-     const errorCode = error.code;
-     const errorMessage = error.message;
-     setErrorMessage(errorCode + "-" + errorMessage);
-
-
-     // ..
-     });
-
-    }
-    else{
-
-        signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+    if (!isSignInForm) {
+      // Sign Up Logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
         .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        console.log(user);
-        // ...
-       })
-       .catch((error) => {
-       const errorCode = error.code;
-       const errorMessage = error.message;
-       setErrorMessage(errorCode + "-" + errorMessage); 
-
-       });
-
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: "https://avatars.githubusercontent.com/u/66188432?v=4",
+          })
+            .then(() => {
+                const { uid, email, displayName, photoURL } = auth.currentUser;  //fetch the user from updated value of auth
+                dispatch(
+                    addUser({
+                    uid: uid,
+                    email: email,
+                    displayName: displayName,
+                    photoURL: photoURL,
+                    })
+                );
+              navigate("/browse");   //once profile updated then navigate
+            })
+            .catch((error) => {
+              setErrorMessage(error.message);
+            });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      // Sign In Logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
     }
-
-
- }
+  };
     
   return (
     <div>
@@ -80,7 +93,7 @@ const Login = () => {
         </div>
         <form onSubmit={(e)=>e.preventDefault()} className='absolute w-3/12 bg-black p-12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
             <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-            { !isSignInForm && <input type="text" placeholder='Enter Name' className="p-4 my-4 w-full bg-gray-700 rounded-lg"/>}
+            { !isSignInForm && <input ref={name}  type="text" placeholder='Enter Name' className="p-4 my-4 w-full bg-gray-700 rounded-lg"/>}
             <input ref={email} type="text" placeholder='Email or phone number' className="p-4 my-4 w-full bg-gray-700 rounded-lg"/>
             <input ref={password} type="password" placeholder='Password' className="p-4 my-4 w-full bg-gray-700 rounded-lg"/>
             <button className='p-4 my-6 bg-red-700 w-full rounded-lg' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
